4cf0e4e879bcfac0a80c4fa4ff0023db0b511bba
EricR <rasliche@gmail.com>
2018-05-26 15:16:58 -0400

    Initial Commit

---
+++ src/components/Zone.vue
@@ -0,0 +1,48 @@
+<template lang="html">
+<div>
+    <h1>Marine Zone Map</h1>
+    <div class="buoy-map"></div>
+</div>
+</template>
+
+<script>
+import state from '@/store'
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    mounted: function() {
+        console.log('mounted');
+        const mapElement = document.querySelector('.buoy-map');
+
+        const options = {
+            zoom: 16,
+            center: new google.maps.LatLng(24.452441, -81.876800),
+            mapTypeId: 'satellite',
+            disableDefaultUI: true,
+            gestureHandling: 'none'
+        }
+
+        const map = new google.maps.Map(mapElement, options);
+
+        this.state.buoyCoordinates.forEach((coord) => {
+            const position = new google.maps.LatLng(coord.lat, coord.lon);
+            const marker = new google.maps.Marker({
+                position,
+                map
+            });
+        });
+    }
+}
+</script>
+
+<style lang="css">
+    .buoy-map {
+        background-color: blue;
+        height: 500px;
+        width: 500px;
+    }
+</style>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+      buoyCoordinates: [{
+          lat: 24.452314,
+          lon: -81.874989
+      }, {
+          lat: 24.452353,
+          lon: -81.877551
+      },{
+          lat: 24.452398,
+          lon: -81.879716
+      }, {
+          lat: 24.451870,
+          lon: -81.878599
+      }]
   },
   mutations: {
 
